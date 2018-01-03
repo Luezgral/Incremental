338a81c541417ce82174d0c17238777870d5ec8e
earlj <earlj@DESKTOP-OP438DM.fios-router.home>
2018-01-02 22:34:10 -0500

    Added "Hello World!"

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -19,6 +20,6 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    return 0;
+    std::cout << "Hello World!";
 }
 
